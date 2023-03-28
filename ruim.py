c362546a74d9c5f62c5a2ac1e96a14a89de8431f
Samara Fadigas <99447521+sdfadigas@users.noreply.github.com>
2023-03-27 21:13:37 -0300

    Add files via upload

--- ruim.py
+++ ruim.py
@@ -18,25 +18,10 @@ class Cat:
         return "miau!"
 
 
-class AnimalSound:
-    def __init__(self, animal):
-        self.animal = animal
 
-    def make_sound(self):
-        return self.animal.speak()
-
-# Pode-se criar novas instâncias sem alterar a classe Animal
-# Cria instâncias das classes Dog e Cat
 dog = Dog("Belinha")
 cat = Cat("Jujuba")
 
-# Cria instâncias da classe AnimalSound passando as instâncias das classes Dog e Cat
-dog_sound = AnimalSound(dog)
-cat_sound = AnimalSound(cat)
-
-# Imprime o som que cada animal faz
-print(dog_sound.make_sound())  
-print(cat_sound.make_sound())  
 
 
 # A classe Animal é uma abstração que representa um animal genérico e possui um método speak() que é implementado pelas classes Dog e Cat. A classe AnimalSound depende da classe Animal, que é uma abstração, e não das classes Dog e Cat. Isso significa que, se você quiser adicionar uma nova classe que represente um animal diferente, você só precisa implementar o método speak() na nova classe e a classe AnimalSound poderá usá-la sem alterações.
